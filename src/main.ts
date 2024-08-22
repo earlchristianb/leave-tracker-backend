73e18ba4dac65391be4adee4adaaaf2c8c285f4a
earlchristianb <earlchristianb@gmail.com>
2024-08-23 00:38:33 +0800

    [Earl]Allow access from everywhere since we have protection implemented

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -12,6 +13,10 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
+  const corsOptions: CorsOptions = {
+    origin: '*', // Allow access from anywhere
+  };
+  app.enableCors(corsOptions);
   setupSwagger(app);
   await app.listen(4200);
 }
